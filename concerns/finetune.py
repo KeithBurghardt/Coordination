efacda45e918df9ee6d45d44c6b2c19ce297f740
Keith Burghardt <keithab@isi.edu>
2023-05-13 23:27:16 -0700

    Add files via upload

---
+++ concerns/finetune.py
@@ -0,0 +1,126 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
+import torch
+import pandas as pd
+import numpy as np
+import pickle
+from transformers import TrainingArguments, Trainer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import EarlyStoppingCallback
+from torch.utils.data import Dataset, DataLoader
+import os
+
+if torch.cuda.is_available():
+    device=torch.device('cuda:0')
+else:
+    device=torch.device('cpu')
+#device = torch.device(﻿"cuda:0" if torch.cuda.is_available(﻿) else "cpu"﻿)
+print(device)
+
+# Create torch dataset
+class Dataset(Dataset):
+    def __init__(self, encodings, labels=None):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        if self.labels:
+            item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.encodings["input_ids"])
+
+def compute_metrics(p):
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
+
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall = recall_score(y_true=labels, y_pred=pred)
+    precision = precision_score(y_true=labels, y_pred=pred)
+    f1 = f1_score(y_true=labels, y_pred=pred)
+
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+
+def test_metrics(pred,labels):
+    accuracy = accuracy_score(y_true=labels, y_pred=pred)
+    recall = recall_score(y_true=labels, y_pred=pred)
+    precision = precision_score(y_true=labels, y_pred=pred)
+    f1 = f1_score(y_true=labels, y_pred=pred)
+
+    return {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1}
+
+
+# Read data
+categories = ['concerns_democracy',
+ 'concerns_economy',
+ 'concerns_environment',
+ 'concerns_fake_news/misinfo',
+ 'concerns_immigration/refugees',
+ 'concerns_international_alliances',
+ 'concerns_national_identity_&_pride',
+ 'concerns_relationship_w/_russia',
+ 'concerns_religion',
+ 'concerns_terrorism/counterterrorism']
+for label in categories:
+    data = pd.read_csv("/data/ashreyas/eval_train_df.csv")
+    tdata = pd.read_csv("/data/ashreyas/eval_test_df.csv")
+    tdata=tdata.dropna()
+    data=data.dropna()
+    model_name = "./bertweetfr-base"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2).to(device)
+
+    # ----- 1. Preprocess data -----#
+    # Preprocess data
+    x = data["cleaned_text"].tolist()
+    y = data[label].tolist()
+    test_x=tdata['cleaned_text'].tolist()
+    test_y=tdata[label].tolist()
+
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.1,random_state=0)
+
+
+    x_train_tokenized = tokenizer(x_train, padding=True, truncation=True, max_length=512)
+    x_val_tokenized = tokenizer(x_val, padding=True, truncation=True, max_length=512)
+    x_test_tokenized = tokenizer(test_x, padding=True, truncation=True, max_length=512)
+
+    train_dataset = Dataset(x_train_tokenized, y_train)
+    val_dataset = Dataset(x_val_tokenized, y_val)
+    test_dataset= Dataset(x_test_tokenized,test_y)
+
+    name=label.replace(' ','_')
+    name=label.replace('/','_')
+    if not os.path.exists('/data/ashreyas/Incas/'+name+'/'):
+        os.makedirs('/data/ashreyas/Incas/'+name+'/')
+
+    # Define Trainer
+    args = TrainingArguments(
+        output_dir='/data/ashreyas/Incas/'+name+'/',
+        evaluation_strategy="steps",
+        save_steps=50,
+        eval_steps=50,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        num_train_epochs=3,
+        seed=7,
+        load_best_model_at_end=True,
+    )
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+    )
+
+    trainer.train()
+    test_dataset = Dataset(x_test_tokenized)
+    raw_pred, _, _ = trainer.predict(test_dataset)
+    y_pred_proba=raw_pred[:,1]
+
+    res=pd.DataFrame(columns=['prediction'])
+    res['prediction']=y_pred_proba
+    res.to_csv('/data/ashreyas/Incas/'+name+'.csv',index=False)
---
+++ concerns/predict_new.py
@@ -0,0 +1,208 @@
+import pandas as pd
+import spacy
+import re
+from string import digits
+import numpy as np
+import time
+from tqdm import tqdm
+import torch
+from transformers import TrainingArguments, Trainer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import EarlyStoppingCallback
+from torch.utils.data import Dataset, DataLoader
+import os
+import math
+import logging
+import pickle
+logging.disable(logging.WARNING)
+
+if torch.cuda.is_available():
+    device=torch.device('cuda')
+else:
+    device=torch.device('cpu')
+#device = torch.device(﻿"cuda:0" if torch.cuda.is_available(﻿) else "cpu"﻿)
+
+return_list=[]
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+class Dataset(Dataset):
+    def __init__(self, encodings, labels=None):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        if self.labels:
+            item["labels"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.encodings["input_ids"])
+
+
+''' Function to generate annotation object that will be returned '''
+def generate_annotation_object(tweetids,text,annot_type,annots):
+    #result=manager.dict()
+    results=[]
+    for i in range(len(tweetids)):
+        result={}
+        result["id"]=tweetids[i]
+        result["type"]=annot_type
+        result["text"]=text
+        result["confidence"]=annots[i]
+        result["providerName"]="ta1-usc-isi"
+        results.append(result)
+    return results
+
+def terror_annotate(ids,test_dataset):
+    global return_list
+    terr_model_path = "/project/lerman_316/ashwin/new_models/concerns_terrorism_counterterrorism/checkpoint-400/"
+    terr_model = AutoModelForSequenceClassification.from_pretrained(terr_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(terr_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del terr_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Terrorism and Counterterrorism","concern-3.2",raw_confidence))
+
+
+def econ_annotate(ids,test_dataset):
+    global return_list
+    eco_model_path = "/project/lerman_316/ashwin/new_models/concerns_economy/checkpoint-400/"
+    eco_model = AutoModelForSequenceClassification.from_pretrained(eco_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(eco_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del eco_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Economy","concern-3.1",raw_confidence))
+
+
+def rel_annotate(ids,test_dataset):
+    global return_list
+    rel_model_path = "/project/lerman_316/ashwin/new_models/concerns_religion/checkpoint-400/"
+    rel_model = AutoModelForSequenceClassification.from_pretrained(rel_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(rel_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del rel_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Religion","concern-3.3",raw_confidence))
+
+def immi_annotate(ids,test_dataset):
+    global return_list
+    immi_model_path = "/project/lerman_316/ashwin/new_models/concerns_immigration_refugees/checkpoint-400/"
+    immi_model = AutoModelForSequenceClassification.from_pretrained(immi_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(immi_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del immi_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Immigration and Refugees","concern-3.4",raw_confidence))
+
+def russ_annotate(ids,test_dataset):
+    global return_list
+    russ_model_path = "/project/lerman_316/ashwin/new_models/concerns_relationship_w__russia/checkpoint-400/"
+    russ_model = AutoModelForSequenceClassification.from_pretrained(russ_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(russ_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del russ_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Relationship with Russia","concern-3.6",raw_confidence))
+
+def cli_annotate(ids,test_dataset):
+    global return_list
+    cli_model_path = "/project/lerman_316/ashwin/new_models/concerns_environment/checkpoint-200/"
+    cli_model = AutoModelForSequenceClassification.from_pretrained(cli_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(cli_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del cli_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Environment and Climate Change","concern-3.8",raw_confidence))
+
+
+def natl_annotate(ids,test_dataset):
+    global return_list
+    natl_model_path = "/project/lerman_316/ashwin/new_models/concerns_national_identity_&_pride/checkpoint-400/"
+    natl_model = AutoModelForSequenceClassification.from_pretrained(natl_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(natl_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del natl_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"National Identity and National Pride","concern-3.7",raw_confidence))
+
+def intl_annotate(ids,test_dataset):
+    global return_list
+    intl_model_path = "/project/lerman_316/ashwin/new_models/concerns_international_alliances/checkpoint-350/"
+    intl_model = AutoModelForSequenceClassification.from_pretrained(intl_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(intl_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del intl_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"International Alliance Organizations","concern-3.5",raw_confidence))
+
+
+def fake_annotate(ids,test_dataset):
+    global return_list
+    fake_model_path = "/project/lerman_316/ashwin/new_models/concerns_fake_news_misinfo/checkpoint-200/"
+    fake_model = AutoModelForSequenceClassification.from_pretrained(fake_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(fake_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del fake_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Fake News/Misinformation","concern-3.9",raw_confidence))
+
+def dem_annotate(ids,test_dataset):
+    global return_list
+    dem_model_path = "/project/lerman_316/ashwin/new_models/concerns_democracy/checkpoint-400/"
+    dem_model = AutoModelForSequenceClassification.from_pretrained(dem_model_path, num_labels=2).to(device)
+    test_trainer = Trainer(dem_model)
+    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    raw_pred, _, _=test_trainer.prediction_loop(test_loader, description="prediction")
+    raw_confidence = raw_pred[:,1]
+    raw_confidence = [sigmoid(x) for x in raw_confidence]
+    del dem_model
+    torch.cuda.empty_cache()
+    return_list.extend(generate_annotation_object(ids,"Democracy","concern-3.11",raw_confidence))
+
+
+df=pd.read_csv('/project/lerman_316/ashwin/AllCombinedTwitterData+text_cleaned.csv')
+print('file read')
+df=df[['id','cleaned_text']]
+df=df.dropna()
+func_dict=[terror_annotate,econ_annotate,rel_annotate,immi_annotate,russ_annotate,cli_annotate,natl_annotate,intl_annotate,fake_annotate,dem_annotate]
+ids=df['id'].tolist()
+tweet_text=df['cleaned_text'].tolist()
+tokenizer = AutoTokenizer.from_pretrained("./bertweetfr-base")
+x_test_tokenized = tokenizer(tweet_text, padding=True, truncation=True, max_length=256)
+test_dataset = Dataset(x_test_tokenized)
+
+for f in func_dict:
+    f(ids,test_dataset)
+with open('/project/lerman_316/ashwin/icwsm_eval.pkl','wb') as fi:
+    pickle.dump(return_list,fi)
