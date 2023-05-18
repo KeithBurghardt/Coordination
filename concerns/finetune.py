2f820b4bf89d6eb57a22152d147da15d3f95f6e3
Keith Burghardt <keithab@isi.edu>
2023-05-17 23:38:41 -0700

    Update finetune.py

--- concerns/finetune.py
+++ concerns/finetune.py
@@ -14,7 +14,6 @@ if torch.cuda.is_available():
     device=torch.device('cuda:0')
 else:
     device=torch.device('cpu')
-#device = torch.device(﻿"cuda:0" if torch.cuda.is_available(﻿) else "cpu"﻿)
 print(device)
 
 # Create torch dataset
